2815cd342ccda536392e8285b0f27af5af6e45ab
Fabian Kochem <fkochem@gmail.com>
2017-02-25 22:56:35 +0100

    Add a task supervisor for socket connections

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -11,7 +11,9 @@ defmodule KVServer do
 
   defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    spawn fn -> serve(client) end
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn ->
+        serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
     loop_acceptor(socket)
   end
 
